3407030c6f1e23080499b8c55733896d154790c0
DaviBorges <davi.paula@hotmail.com>
2025-02-07 17:52:44 -0300

    Criando Model View de Usuario

--- src/Domain/ModelViews/UserModelViews.cs
+++ src/Domain/ModelViews/UserModelViews.cs
@@ -1,13 +1,15 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using System.Threading.Tasks;
 
 namespace AcologAPI.src.Domain.ModelViews
 {
-    public record UserModelViews
+    public class UserModelView
     {
-        
-
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string Token { get; set; }
     }
 }
